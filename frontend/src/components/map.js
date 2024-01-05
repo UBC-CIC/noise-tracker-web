30a893e4fb392c02ce8b1024d3533f86c240e4a0
Liam Driscoll <liamur.driscoll@hotmail.com>
2024-01-05 15:44:45 -0800

    Add icons with popups to the map

--- frontend/src/components/map.js
+++ frontend/src/components/map.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Icon } from 'leaflet';
+import { Button, Typography } from '@mui/material';
 import 'leaflet/dist/leaflet.css';
+
+const hydrophoneIcon = new Icon({
+  iconUrl: require("./hydrophoneIcon.png"),
+  iconSize: [35,35]
+})
 
 export default function Map() {
   const position = [49.2608724, -123.113952]; // Initial map position
@@ -11,6 +18,12 @@ export default function Map() {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
+      <Marker position={position} icon={hydrophoneIcon}> 
+        <Popup>
+          <Typography>Hydrophone</Typography>
+          <Button>View Analytics</Button>
+        </Popup>
+      </Marker>
     </MapContainer>
   );
 };
