5f5a4f62a89a8e77096b3b84c1e0c104bd22adbf
Liam Driscoll <liamur.driscoll@hotmail.com>
2024-01-05 16:03:32 -0800

    Add navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
 
+import NavBar from './components/navbar';
 import InteractiveMap from './pages/InteractiveMap';
 
 function App() {
   return (
     <Router>
+      <NavBar/>
       <Routes>
         <Route path="/" element={<InteractiveMap/>}/>
       </Routes>
---
+++ frontend/src/components/navbar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+const NavBar = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          NoiseTracker
+        </Typography>
+        <Button color="inherit" component={Link} to="/home">
+          Home
+        </Button>
+        <Button color="inherit" component={Link} to="/contact">
+          Contact
+        </Button>
+        <Button color="inherit" component={Link} to="/">
+          Map
+        </Button>
+        <Button color="inherit" component={Link} to="/login">
+          Login
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default NavBar;
