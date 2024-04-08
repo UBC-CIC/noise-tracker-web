2925f19ebf77037ef8858237524ef2827fb9956a
Liam Driscoll <liamur.driscoll@hotmail.com>
2024-04-08 16:16:41 -0700

    Add content to sidebar trends tab

--- frontend/src/components/sidebarTrends.js
+++ frontend/src/components/sidebarTrends.js
@@ -3,8 +3,11 @@ import { Typography } from "@mui/material";
 export default function SidebarTrends(){
     return(
         <div style={{ paddingLeft: '20px' }}>
-            <Typography variant="h5">
-                Sidebar Trends
+            <Typography className="sidebar-typography-padding">
+            Longer-term noise trends will become available in the coming months.
+            </Typography>
+            <Typography className="sidebar-typography-padding">
+                <strong>Learn more about tracking underwater noise trends in our Education Hub.</strong>
             </Typography>
         </div>
     );
