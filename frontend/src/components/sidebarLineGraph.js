41a01e22cada6361cf16546528ec97f47496392d
Liam Driscoll <liamur.driscoll@hotmail.com>
2024-04-22 17:08:04 -0700

    Refactor line graph to handle no data and plotting errors

--- frontend/src/components/sidebarLineGraph.js
+++ frontend/src/components/sidebarLineGraph.js
@@ -1,52 +1,78 @@
 import { Typography, CircularProgress } from '@mui/material';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Plot from 'react-plotly.js';
 
 const SidebarLineGraph = ({ splData, loading }) => {
-  // Check if splData is defined
-  if (!splData && !loading) {
-    return (
-      <div>
-          <Typography className='sidebar-typography-padding'>No Sound Pressure Level Data Available</Typography>
-      </div>
-      );
-  }
-
-  // Extracting dates and values for each trace
-  const dates = splData?.data.map(data => data.date);
-  const values = splData?.data.map(data => data.values);
-  
-  // Calculate least f_min and greatest f_max
-  let lowerFrequency = Infinity;
-  let upperFrequency = -Infinity;
-  values[0].forEach(value => {
-    // Convert to numbers before comparing
-    let f_min = Number(value.f_min);
-    let f_max = Number(value.f_max);
-
-    if (f_min < lowerFrequency) {
-      lowerFrequency = f_min;
-    }
-    if (f_max > upperFrequency) {
-      upperFrequency = f_max;
-    }
-  });
-
-  // Creating traces
-  const traces = values[0].map((_, index) => ({
-    x: dates,
-    y: values.map(data => 10*Math.log10(data[index].val)),
-    name: `${Number(values[0][index].f_min)} to ${Number(values[0][index].f_max)} Hz`,
-    type: 'scatter',
-  }));
+  const [lowerFrequency, setLowerFrequency] = useState(null);
+  const [upperFrequency, setUpperFrequency] = useState(null);
+  const [traces, setTraces] = useState(null);
+  const [error, setError] = useState(null);
 
   const config = {
     responsive: true
   };
 
+  useEffect(() => {
+    if (!splData && !loading) {
+      setError("No Sound Pressure Level Data Available");
+      return;
+    }
+
+    // Extracting dates and values for each trace
+    const dates = splData?.data.map(data => data.date);
+    const values = splData?.data.map(data => data.values);
+
+    const calculateFrequencyRange = frequencies => {
+      // Calculate least f_min and greatest f_max
+      let lowerFrequency = Infinity;
+      let upperFrequency = -Infinity;
+      values[0].forEach(value => {
+        // Convert to numbers before comparing
+        let f_min = Number(value.f_min);
+        let f_max = Number(value.f_max);
+
+        if (f_min < lowerFrequency) {
+          lowerFrequency = f_min;
+        }
+        if (f_max > upperFrequency) {
+          upperFrequency = f_max;
+        }
+      });
+      return { lowerFrequency, upperFrequency };
+    };
+
+    const calculateTraces = (dates, values) => {
+      // Creating traces
+      const traces = values[0].map((_, index) => ({
+        x: dates,
+        y: values.map(data => 10*Math.log10(data[index].val)),
+        name: `${Number(values[0][index].f_min)} to ${Number(values[0][index].f_max)} Hz`,
+        type: 'scatter',
+      }));
+      return traces;
+    };
+
+    if (!loading && dates && values){
+      try {
+        const { lowerFrequency, upperFrequency } = calculateFrequencyRange(values);
+        const traces = calculateTraces(dates, values);
+        setLowerFrequency(lowerFrequency);
+        setUpperFrequency(upperFrequency);
+        setTraces(traces);
+        setError(null);
+      } catch (error) {
+        setError("Error plotting data.");
+      }
+    }
+  }, [splData, loading]);
+
+  console.log("Error: ", error);
+
   return (
     <div>
-      {loading ? ( // Render circular progress if loading is true
+      {error ? (
+        <Typography className='sidebar-typography-padding'>{error}</Typography>
+      ) : (loading || !traces) ? ( // Render circular progress if loading is true
                 <center>
                     <CircularProgress color="success" />
                 </center>
@@ -90,7 +116,7 @@ const SidebarLineGraph = ({ splData, loading }) => {
               Learn more about Sound Pressure Levels as a tool to understanding the ocean soundscape in our Education Hub.
             </Typography>
             </div>
-          )}
+      )}
     </div>
   );
 };
